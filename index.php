29d057d38701141cfdf6dd91f50d4dfc59aa0f49
BenTG <ben@stormbox.com.au>
2023-01-31 16:47:26 +0800

    Tweak
    Addition of placeholder images

--- index.php
+++ index.php
@@ -13,12 +13,12 @@ get_header(); ?>
 				</a>
 			</div>
 		<?php } else { ?>
-			<img alt="Jungle Flora Hero Image" src="https://via.placeholder.com/960x960.png">
+			<img alt="Jungle Flora Hero Image" src="<?php echo get_template_directory_uri();?>-child/assets/images/HeroSliderM.jpg">
 		<?php } ?>	
 		</div>
 		<div class="bt-flex">
-			<div class="bt-1-2"><img alt="" src="https://via.placeholder.com/800x600.png"></div>
-			<div class="bt-1-2"><img alt="" src="https://via.placeholder.com/800x600.png"></div>
+			<div class="bt-1-2"><img alt="" src="<?php echo get_template_directory_uri();?>-child/assets/images/placeholder1.jpg"></div>
+			<div class="bt-1-2"><img alt="" src="<?php echo get_template_directory_uri();?>-child/assets/images/placeholder2.jpg"></div>
 		</div>
 
 		<div class="bt-flex">
@@ -26,7 +26,7 @@ get_header(); ?>
 				<h2>About Jungle Flora</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat posuere sollicitudin. Vestibulum blandit elit sit amet quam lobortis, eu mattis tortor volutpat. Etiam eget magna at urna maximus vestibulum ac at ante. Etiam ultricies tristique sapien non tristique. Donec faucibus libero in ex faucibus, vel condimentum nisi imperdiet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut quis euismod urna.</p>
 			</div>
-			<div class="bt-1-2"><img alt="" src="https://via.placeholder.com/800x600.png"></div>
+			<div class="bt-1-2"><img alt="" src="<?php echo get_template_directory_uri();?>-child/assets/images/placeholder3.jpg"></div>
 		</div>
 
 
