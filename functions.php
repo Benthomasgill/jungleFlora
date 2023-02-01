31e75ff7cd873293f3c1be84c1f7dd8bdc32d641
BenTG <ben@stormbox.com.au>
2023-02-01 11:55:23 +0800

    Tweak
    Asset updates, custom logo set up. WIP Nav set up desktop

---
+++ functions.php
@@ -0,0 +1,57 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+	wp_enqueue_style( 'child-style',
+		get_stylesheet_uri(),
+		array( 'parenthandle' ),
+		wp_get_theme()->get( 'Version' ) // This only works if you have Version defined in the style header.
+	);
+}
+
+// Override Header logo function 
+
+if ( ! function_exists( 'storefront_site_title_or_logo' ) ) {
+	/**
+	 * Display the site title or logo
+	 *
+	 * @since 2.1.0
+	 * @param bool $echo Echo the string or return it.
+	 * @return string
+	 */
+	function storefront_site_title_or_logo( $echo = true ) {
+		if(wp_is_mobile()) {
+			$logo = get_template_directory_uri() . '-child/assets/images/logos/jfstackedlogo.png';
+		} else {
+			$logo = get_template_directory_uri() . '-child/assets/images/logos/jflogo.png';
+		}	
+		$html = '<h1 class="logo"><img src="' . $logo . '"></h1>';
+
+		if ( ! $echo ) {
+			return $html;
+		}
+
+		echo $html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	}
+}
+
+
+
+if ( ! function_exists( 'storefront_product_search' ) ) {
+	/**
+	 * Display Product Search
+	 *
+	 * @since  1.0.0
+	 * @uses  storefront_is_woocommerce_activated() check if WooCommerce is activated
+	 * @return void
+	 */
+	function storefront_product_search() {
+		if ( storefront_is_woocommerce_activated() ) {
+			?>
+			<div class="site-search">
+				<?php the_widget( 'WC_Widget_Product_Search', 'title=' ); ?>
+			</div>
+			<?php
+		}
+	}
+}
+
